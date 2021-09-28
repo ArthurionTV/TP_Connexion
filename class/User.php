6212ded1b4afb1dabeb3cb52482cbefa0cad39a3
Arthurion <a.aquiert.pro@gmail.com>
2021-09-28 13:22:33 +0200

    11e Commit

--- class/User.php
+++ class/User.php
@@ -1,15 +1,103 @@
 <?php
 
-class User{
-    public $id;
-    public $mail;
-    public $pseudo ;
-    public $password;
+class User
+{
 
-public function __construct() {}
-public function getmail(){}
-public function getpseudo(){}
-public function getpassword(){}
-public function getid(){}
+    private $id;
+    private $mail;
+    private $pseudo;
+    private $password;
+
+    public function __construct($valeurs = array())
+    {
+        foreach ($valeurs as $key => $value) {
+            // On récupère dynamiquement le nom du setter correspondant à l'attribut.
+            $method = 'set' . ucfirst($key);
+            // Si le setter correspondant existe.
+            if (method_exists($this, $method)) {
+                // On appelle le setter dynamiquement.
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of mail
+     */ 
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set the value of mail
+     *
+     * @return  self
+     */ 
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pseudo
+     */ 
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * Set the value of pseudo
+     *
+     * @return  self
+     */ 
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
 }
-?>
--- includes/signup.php
+++ includes/signup.php
@@ -9,9 +9,7 @@ include './connexion_bdd.php';
 
 <head>
     <!-- lier bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" 
-    rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
-    crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -41,16 +39,16 @@ include './connexion_bdd.php';
         </div>
         <button type="submit" class="btn btn-primary">Enregistrer</button> <a href="./index.php">Revenir à l'accueil</a>
     </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" 
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 
 </html>
 
 <?php
-//initialiser le tableau $tab
-$tab = $_SESSION["ListUser"];
+$userDao = new UserDao($db); // instanciation de UserDao
+$tab = $userDao->getAll(); // recup du tab user
+
+var_dump($tab);
 // requête serveur 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //  on vérifie si les champs sont remplis
@@ -59,26 +57,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // on vérifie avec la méthode si l'email est correctement écris ("@" et ".fr" par exemple)
         if (filter_var($_POST["email1"], FILTER_VALIDATE_EMAIL)) {
             //  on parcour le tableau pour ensuite vérifier si l'émail existe déjà
-            foreach ($_SESSION["ListUser"] as $i) {
-                if (in_array($_POST["email1"], $tab[$i]->getMail())) {
+            for ($i = 0; $i < count($tab); $i++) {
+                if ($_POST["email1"] == $tab[$i]->getMail()) {
                     $mailValid = false;
                     $error = "Mail déjà utilisé";
                     break;
                 }
             }
+
+
+
             // si l'email est valide et disponible, on vérifie ensuite les mots de passes 
             if ($mailValid == true) {
                 if ($_POST["password1"] === $_POST["password2"]) {
                     //enregistrement de l'utilisateur dans le tableau
                     $tab = [
-                        $_POST["mail"],
-                        password_hash($_POST["password"], PASSWORD_DEFAULT),
-                        htmlspecialchars($_POST["pseudo"])
+                        "mail" => $_POST["email1"],
+                        "password" => password_hash($_POST["password1"], PASSWORD_DEFAULT),
+                        "pseudo" => htmlspecialchars($_POST["username1"])
                     ];
-                    $_SESSION["ListUser"] = $tab;
+                    var_dump($tab);
                     //instanciation de l'objet user
-                    $user = $user[$tab];
-                    $user = new UserDao();
+                    $user = new User($tab);
+                    var_dump($user);
+                    $userDao->add($user);
                     // redirection à la page d'accueil
                     header("Location: ./index.php");
                 } else {
